9610216b486969bde4d1180777581cee982cdfc5
Gitle Mikkelsen <gitlem@gmail.com>
2017-01-29 20:47:08 +0100

    safety

--- GMopen.cpp
+++ GMopen.cpp
@@ -2,9 +2,15 @@
 
 GMEXPORT double OpenInExplorer(char* path) 
 {
-	return (double)(int)ShellExecuteW(NULL, L"open", ConvertCharArrayToRawLPCWSTR(path), NULL, NULL, SW_SHOW);
+	if (path == NULL)
+		return 0.;
+	wchar_t* pathw = ConvertCharArrayToRawLPCWSTR(path);
+	int result = (int)ShellExecuteW(NULL, L"open", pathw, NULL, NULL, SW_SHOW);
+	delete pathw;
+	return (double)result;
 }
 	
+//credits to yal.cc for this
 GMEXPORT double GetColor(double c) { 
 	CHOOSECOLOR cc;
 	ZeroMemory(&cc, sizeof(cc));
@@ -21,6 +27,8 @@ GMEXPORT double GetColor(double c) {
 
 GMEXPORT double ShowMessage(char* message, char* caption, double flags)
 {
+	if ((message == NULL) || (caption == NULL))
+		return 0.;
 	std::thread messageThread(ShowMessageThreaded, message, caption, (unsigned int)(flags + 0.5));
 	messageThread.detach();
 	return 1.;
@@ -28,6 +36,8 @@ GMEXPORT double ShowMessage(char* message, char* caption, double flags)
 
 GMEXPORT double ShowQuestion(char* message, char* caption, double flags)
 {
+	if ((message == NULL) || (caption == NULL))
+		return 0.;
 	int id = dialogId++;
 	std::thread messageThread(ShowQuestionThreaded, message, caption, (unsigned int)(flags+0.5), id);
 	messageThread.detach();
@@ -36,12 +46,20 @@ GMEXPORT double ShowQuestion(char* message, char* caption, double flags)
 
 void ShowMessageThreaded(char* message, char* caption, unsigned int flags)
 {
+	wchar_t* msg = ConvertCharArrayToLPCWSTR(message);
+	wchar_t* cap = ConvertCharArrayToLPCWSTR(caption);
 	MessageBoxW(NULL, ConvertCharArrayToLPCWSTR(message), ConvertCharArrayToLPCWSTR(caption), MB_OK | flags);
+	delete msg;
+	delete cap;
 }
 
 void ShowQuestionThreaded(char* message, char* caption, unsigned int flags, int id)
 {
-	int result = MessageBoxW(NULL, ConvertCharArrayToLPCWSTR(message), ConvertCharArrayToLPCWSTR(caption), MB_YESNO | flags);
+	wchar_t* msg = ConvertCharArrayToLPCWSTR(message);
+	wchar_t* cap = ConvertCharArrayToLPCWSTR(caption);
+	int result = MessageBoxW(NULL, msg, cap, MB_YESNO | flags);
+	delete msg;
+	delete cap;
 	int resultMap = CreateDsMap(0);
 	DsMapAddDouble(resultMap, "id", (double)id);
 	DsMapAddDouble(resultMap, "status", (double)(result == IDYES));
@@ -50,13 +68,24 @@ void ShowQuestionThreaded(char* message, char* caption, unsigned int flags, int 
 
 GMEXPORT double FileCopy(char* source, char* dest)
 {
-	return (double)(CopyFileW(ConvertCharArrayToRawLPCWSTR(source), ConvertCharArrayToRawLPCWSTR(dest), false) == TRUE);
+	if ((source == NULL) || (dest == NULL))
+		return 0.;
+	wchar_t* sourcew = ConvertCharArrayToRawLPCWSTR(source);
+	wchar_t* destw = ConvertCharArrayToRawLPCWSTR(dest);
+	double result = (double)(CopyFileW(sourcew, destw, false) == TRUE);
+	delete sourcew;
+	delete destw;
+	return result;
 }
 
 GMEXPORT double FileExists(char* path)
 {
+	if (path == NULL)
+		return 0.;
 	WIN32_FIND_DATAW FindFileData;
-	HANDLE handle = FindFirstFileW(ConvertCharArrayToRawLPCWSTR(path), &FindFileData);
+	wchar_t* pathw = ConvertCharArrayToRawLPCWSTR(path);
+	HANDLE handle = FindFirstFileW(pathw, &FindFileData);
+	delete pathw;
 	if (handle == INVALID_HANDLE_VALUE)
 		return 0.;
 	else
@@ -65,20 +94,29 @@ GMEXPORT double FileExists(char* path)
 
 wchar_t *ConvertCharArrayToLPCWSTR(char* charArray)
 {
-	for (int i = 0; i < 4096; i++)
+	int i = 0;
+	for (i = 0; i < 4096; i++)
 	{
+		if (charArray[i] == '\0')
+			break;
 		if (charArray[i] == '#')
 			charArray[i] = '\n';
 	}
-	wchar_t* wString = new wchar_t[4096];
-	MultiByteToWideChar(CP_ACP, 0, charArray, -1, wString, 4096);
+	wchar_t* wString = new wchar_t[i+1];
+	MultiByteToWideChar(CP_ACP, 0, charArray, -1, wString, i + 1);
     return wString;
 }
 
 wchar_t *ConvertCharArrayToRawLPCWSTR(char* charArray)
 {
-	wchar_t* wString = new wchar_t[4096];
-	MultiByteToWideChar(CP_ACP, 0, charArray, -1, wString, 4096);
+	int i = 0;
+	for (i = 0; i < 4096; i++)
+	{
+		if (charArray[i] == '\0')
+			break;
+	}
+	wchar_t* wString = new wchar_t[i+1];
+	MultiByteToWideChar(CP_ACP, 0, charArray, -1, wString, i+1);
 	return wString;
 }
 
